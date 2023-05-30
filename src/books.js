392a2d9b9cbac2466d29a80b1af192a3af84e730
vishal-gits <123520641+vishal-gits@users.noreply.github.com>
2023-05-30 06:17:11 +0530

    Best Selling Books- project upload

---
+++ src/books.js
@@ -0,0 +1,11 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+import img4 from "./images/book4.jpg";
+
+export const books = [
+  { img: img1, title: "Happiness is Success", author: "Atman in Ravi", id: 1 },
+  { img: img2, title: "Nala Dayamanti", author: "Anand Neelaknatan", id: 2 },
+  { img: img3, title: "The Abyss", author: "Jeya Mohan", id: 3 },
+  { img: img4, title: "Atomic Habits", author: "James Clear", id: 4 },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { books } from "./books.js";
+import Book from "./Book.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = function () {
+  return (
+    <>
+      <h1>Best Selling Books</h1>
+      <section className="booklist">
+        {books.map((book) => {
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    </>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
