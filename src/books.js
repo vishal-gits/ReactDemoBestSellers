af3a2b421dcfdc7bf65a048a6504e3e79c1adbc3
vishal-gits <123520641+vishal-gits@users.noreply.github.com>
2023-05-30 07:24:39 +0530

    added new images for books

--- src/books.js
+++ src/books.js
@@ -2,10 +2,24 @@ import img1 from "./images/book1.jpg";
 import img2 from "./images/book2.jpg";
 import img3 from "./images/book3.jpg";
 import img4 from "./images/book4.jpg";
+import img5 from "./images/book5.jpg";
+import img6 from "./images/book6.jpg";
+import img7 from "./images/book7.jpg";
+import img8 from "./images/book8.jpg";
+import img9 from "./images/book9.jpg";
+import img10 from "./images/book10.jpg";
+
 
 export const books = [
   { img: img1, title: "Happiness is Success", author: "Atman in Ravi", id: 1 },
   { img: img2, title: "Nala Dayamanti", author: "Anand Neelaknatan", id: 2 },
   { img: img3, title: "The Abyss", author: "Jeya Mohan", id: 3 },
   { img: img4, title: "Atomic Habits", author: "James Clear", id: 4 },
+  { img: img5, title: "Grandma's Bag of stories", author: "Sudha Murthy", id: 5 },
+  { img: img6, title: "Energize your Mind", author: "Gaur Gopal Das", id: 6 },
+  { img: img7, title: "Indian Polity", author: "M Laxmikant", id: 7 },
+  { img: img8, title: "The Power of Your Subconcious Mind", author: "Joseph Murthy", id: 8 },
+  { img: img9, title: "The Hidden Hindu", author: "Akshat Gupta", id: 9 },
+  { img: img10, title: "IKIGAI", author: "Hector Gracia & Francesc Miralles", id: 10 },
+  
 ];
